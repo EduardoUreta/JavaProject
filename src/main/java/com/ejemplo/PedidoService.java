31352d21cd0d41f5e061057c051b44ed17bb7106
Eduardo Ureta <eduardo.uretaca@gmail.com>
2025-06-23 20:26:55 -0400

    .

---
+++ src/main/java/com/ejemplo/PedidoService.java
@@ -0,0 +1,16 @@
+package com.ejemplo;
+
+public class PedidoService {
+
+    private DescuentoRepository repository;
+        public PedidoService(DescuentoRepository repository) {
+        this.repository = repository;
+    }
+
+
+    public double calcularTotal(double subtotal, boolean aplicaDescuento, boolean envioExpress) {
+        double descuento = aplicaDescuento ? 0.1 : 0;
+        double envio = envioExpress ? 20.0 : 10.0;
+        return (subtotal * (1 - descuento)) + envio;
+    }
+}
